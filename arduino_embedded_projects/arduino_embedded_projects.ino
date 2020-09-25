140c89dc3737d148518f8c42bb0ee0ee8487d2d9
Eric Brown <56092687+evvic@users.noreply.github.com>
2020-09-25 12:20:58 +0300

    Joystick calibration & LED pins added
    LCD prints X & Y value of joystick in percentage (-100% - 100%)
    2 LEDs added that respond to joystick movement (dims on percentage):
    LED @pin8 lights for forward & reverse (y-axis).
    LED @pin9 lights for left & right (x-axis).

--- arduino_embedded_projects/arduino_embedded_projects.ino
+++ arduino_embedded_projects/arduino_embedded_projects.ino
@@ -10,28 +10,60 @@
 void setup() {
 
   Serial.begin(9600); // open the serial port at 9600 bps:
-
+  lcd.begin(20,4);
 }
 
  
 
 void loop() {
 
+  int LEDpinDim = 9;
+  int LEDpinDirection = 8;
 
-  int sensorValueX;
-  int sensorValueY;
+
+
+
+
+  ///////////////////////////////////
+  //////   X-Value             //////
+  ///////////////////////////////////
+  //reads about 4.91V, never really hits full 5V
+    float sensorValueX = analogRead(A7);
   
-  sensorValueX = analogRead(A7);
-  sensorValueY = analogRead(A6);
+    int lightX = ((sensorValueX - 491) / 532) * 255;
+    analogWrite(9,abs(lightX)); //LED power is on the longer metal leg
+  
+    sensorValueX = (sensorValueX - 510) / 5.1;
+  
+    int colX = (sensorValueX / 10) + 10;
 
-  //lcd.setCursor(0, 1);
+
+  ///////////////////////////////////
+  //////   Y-Value             //////
+  ///////////////////////////////////
+    float sensorValueY = analogRead(A6);
+  
+    int lightY = ((sensorValueY - 512) / 511) * 255;
+    analogWrite(8, abs(lightY)); //LED power is on the longer metal leg
+    
+    sensorValueY = (sensorValueY - 513) / 5.13;
+
+  lcd.setCursor(0, 0);
   lcd.print("X-value:");
   lcd.print(sensorValueX);
+  lcd.print(" %");
   Serial.print(sensorValueX);
 
-  //lcd.setCursor(0, 2);
+  lcd.setCursor(0, 1);
+  lcd.print("-100%     0%     100%");
+
+  lcd.setCursor(colX, 2);
+  lcd.print("I");
+
+  lcd.setCursor(0, 3);
   lcd.print("Y-value:");
   lcd.print(sensorValueY);
+  lcd.print(" %");
   Serial.print(sensorValueY);
 
   delay(500);
